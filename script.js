4b5b088b32f6e94b580f0a5f13eb20855cdfa87e
neo94 <baitichhamza@gmail.com>
2023-09-09 15:23:31 +0100

    add data to local storage

--- script.js
+++ script.js
@@ -5,20 +5,29 @@ let ticketPrice = +movie.value;
 const count = document.querySelector('.count');
 const total = document.querySelector('.total');
 
+// add movie data to local storage
+function addMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('movieIndex', movieIndex);
+  localStorage.setItem('moviePrice', moviePrice);
+}
+
 // udpate count and total
 function udpateSelectedSeats() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
   const totalSeats = selectedSeats.length;
   count.textContent = totalSeats;
-
   const totalPrice = totalSeats * ticketPrice;
   total.textContent = totalPrice;
+
+  const seatIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+  localStorage.setItem('SelectedSeatsIndex', JSON.stringify(seatIndex));
 }
 
-//change ticket value
+//update ticket value
 movie.addEventListener('change', (e) => {
   ticketPrice = e.target.value;
   udpateSelectedSeats();
+  addMovieData(e.target.selectedIndex, e.target.value);
 });
 
 //add class of selected to selected seats
